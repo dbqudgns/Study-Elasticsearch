21ed643e73db7b635d3a086d5673ac52c5c7893f
dbqudgns <dbqudgns4632@gmail.com>
2025-06-06 14:38:39 +0900

    feat : 기타 파일 세팅

--- elasticsearch-coupang-project/src/main/java/com/example/coupangapiserver/product/ProductRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.coupangapiserver.product;
-
-import com.example.coupangapiserver.product.domain.Product;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProductRepository extends JpaRepository<Product, Long> {
-
-}
